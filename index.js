7f78b49f4d4f83e026ff4ab6a8926f655acdea61
Connor Henn <connor.henn@yahoo.com>
2022-07-18 15:00:54 -0700

    commit tree be43718d01f725159de68d809b691f54a94110a7

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.random()*100;
+random = Math.ceil(random);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(0, 1, 10, 20);
